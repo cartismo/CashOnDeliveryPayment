81691bca821a41408302084fd0b0a3b7d656ca88
pavelpalashturov <pa.palashturov@gmail.com>
2026-03-16 14:18:56 +0200

    Test

--- Config/config.php
+++ Config/config.php
@@ -2,22 +2,4 @@
 
 return [
     'name' => 'CashOnDeliveryPayment',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Default Settings
-    |--------------------------------------------------------------------------
-    */
-    'defaults' => [
-        'enabled' => false,
-        'title' => 'Cash on Delivery',
-        'description' => 'Pay with cash when your order is delivered.',
-        'instructions' => 'Please have the exact amount ready when the courier arrives.',
-        'fee_type' => 'none', // none, fixed, percentage
-        'fee_amount' => 0,
-        'minimum_order_amount' => null,
-        'maximum_order_amount' => null,
-        'order_status' => 'processing',
-        'sort_order' => 0,
-    ],
 ];
